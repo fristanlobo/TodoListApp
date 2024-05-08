f4247996b1bcaeaa0f76618a0623d416a793c373
fristanlobo <fristanlobo@gmail.com>
2024-05-08 23:30:52 +0530

    todo with ts

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../components/styles.css';
+import { Todo } from '../model';
+import SingleTodo from './SingleTodo';
+
+interface TodoListProps {
+    todos: Todo[],
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>,
+}
+
+const TodoList = ({ todos, setTodos }: TodoListProps) => {
+    return (
+        <div className="todos">
+            {
+                todos.map((todo) =>
+                    <SingleTodo
+                        todo={todo}
+                        //key={todo.id}
+                        todos={todos}
+                        setTodos={setTodos}
+                    />
+                )
+            }
+        </div>
+    )
+}
+
+export default TodoList
